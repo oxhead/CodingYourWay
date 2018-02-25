dc7a3c2b0001d5a28c53a08a533aabf4ae1599ab
Chin-Jung Hsu <kmscout@gmail.com>
2018-02-25 12:46:39 -0500

    LeetCode 202.

---
+++ src/lt_202.py
@@ -0,0 +1,56 @@
+"""
+https://leetcode.com/problems/happy-number
+
+Related:
+  - lt_258
+  - lt_263
+
+Complexity:
+  - Time:
+  - Space:
+"""
+
+"""
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 19 is a happy number
+
+    1^2 + 9^2 = 82
+    8^2 + 2^2 = 68
+    6^2 + 8^2 = 100
+    1^2 + 0^2 + 0^2 = 1
+"""
+
+class Solution:
+    def __init__(self):
+        self.records = set()
+
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        s = sum([int(d) ** 2 for d in str(n)])
+        if s == 1: return True
+        else:
+            if s in self.records: return False
+            else:
+                self.records.add(s)
+                return self.isHappy(s)
+
+if __name__ == '__main__':
+    test_cases = [
+        (19, True),
+        (1, True),
+        (0, False),
+        (4, False),
+    ]
+
+    for test_case in test_cases:
+        print('case:', test_case)
+        output = Solution().isHappy(test_case[0])
+        print('output:', output)
+        assert output == test_case[1]
+
