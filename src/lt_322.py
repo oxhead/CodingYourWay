d7f7dd79c72f4b9379469b83c33a0e779264edb1
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-02 00:51:09 -0500

    Update.

--- src/lt_322.py
+++ src/lt_322.py
@@ -32,6 +32,7 @@ class Solution:
         :type amount: int
         :rtype: int
         """
+        # https://tenderleo.gitbooks.io/leetcode-solutions-/content/GoogleMedium/322.html
         sols = [sys.maxsize-1] * (amount+1)
         sols[0] = 0
 
@@ -42,16 +43,36 @@ class Solution:
                 
         return -1 if sols[amount] == sys.maxsize-1 else sols[amount]
 
-
-
+    def coinChange_search(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        # much faster
+        def search(amount, count, index):
+            if amount == 0:
+               self.output = min(self.output, count)
+               return
+            if amount < 0: return
+            for i in range(index, len(coins)):
+                # restric search space
+                if coins[i] <= amount < coins[i] * (self.output - count):
+                    search(amount - coins[i], count + 1, i) 
+        self.output = float('inf')
+        coins.sort(reverse=True)
+        search(amount, 0, 0)
+        return self.output if self.output < float('inf') else -1
+        
 if __name__ == '__main__':
     test_cases = [
-        (([25, 20, 5, 1], 127), 7),
+        #(([1], 0), 0),
+        #(([25, 20, 5, 1], 127), 7),
         (([25, 20, 4, 1], 121), 6),
-        (([50, 45, 10, 2], 3), -1),
-        (([50, 45, 10, 2], 92), 3),
-        (([176, 6, 366, 357, 484, 226, 1, 104, 160, 331], 5557), 13),
-        (([370,417,408,156,143,434,168,83,177,280,117], 9953), 24),
+        #(([50, 45, 10, 2], 3), -1),
+        #(([50, 45, 10, 2], 92), 3),
+        #(([176, 6, 366, 357, 484, 226, 1, 104, 160, 331], 5557), 13),
+        #(([370,417,408,156,143,434,168,83,177,280,117], 9953), 24),
     ]
 
     for test_case in test_cases:
