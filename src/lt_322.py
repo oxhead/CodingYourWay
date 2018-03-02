fd29754b8c10374ccb774ee5d0315906376441ee
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-01 23:47:13 -0500

    Update.

--- src/lt_322.py
+++ src/lt_322.py
@@ -1,6 +1,28 @@
-'''
+"""
 https://leetcode.com/problems/coin-change
-'''
+
+Related:
+
+Complexity:
+  - Time: O()
+  - Space: O()
+"""
+
+"""
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+coins = [1, 2, 5], amount = 11
+return 3 (11 = 5 + 5 + 1)
+
+Example 2:
+coins = [2], amount = 3
+return -1.
+
+Note:
+You may assume that you have an infinite number of each kind of coin. 
+"""
+
 import sys
 
 class Solution:
@@ -10,7 +32,6 @@ class Solution:
         :type amount: int
         :rtype: int
         """
-
         sols = [sys.maxsize-1] * (amount+1)
         sols[0] = 0
 
@@ -25,18 +46,19 @@ class Solution:
 
 if __name__ == '__main__':
     test_cases = [
-        ([25, 20, 5, 1], 127, 7),
-        ([25, 20, 4, 1], 121, 6),
-        ([50, 45, 10, 2], 3, -1),
-        ([50, 45, 10, 2], 92, 3),
-        ([176, 6, 366, 357, 484, 226, 1, 104, 160, 331], 5557, 13)
+        (([25, 20, 5, 1], 127), 7),
+        (([25, 20, 4, 1], 121), 6),
+        (([50, 45, 10, 2], 3), -1),
+        (([50, 45, 10, 2], 92), 3),
+        (([176, 6, 366, 357, 484, 226, 1, 104, 160, 331], 5557), 13),
+        (([370,417,408,156,143,434,168,83,177,280,117], 9953), 24),
     ]
 
     for test_case in test_cases:
-        coins, amount, expected = test_case
-        result = Solution().coinChange(coins, amount)
-        print(coins, amount, expected, result)
-        assert result == expected
+        print('case:', test_case)
+        output = Solution().coinChange(*test_case[0])
+        print('output:', output)
+        assert output == test_case[1]
 
     
         
