d9fd2ce7f2bc38c1ff000679925db5feada46050
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-16 11:14:19 -0400

    Update.

--- src/lt_46.py
+++ src/lt_46.py
@@ -24,8 +24,24 @@ For example,
 ]
 """
 
+import itertools
+
 class Solution:
     def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        output = [[]]
+        for n in nums:
+            tmp = []
+            for sub in output:
+                for i in range(len(sub) + 1):
+                    tmp.append(sub[:i] + [n] + sub[i:])
+            output = tmp
+        return output
+
+    def permute_recursive(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
@@ -37,7 +53,7 @@ class Solution:
 
         output = []
         for i in range(len(nums)):
-            for sub in self.permute(nums[:i] + nums[i+1:]):
+            for sub in self.permute_recursive(nums[:i] + nums[i+1:]):
                 output.append([nums[i]] + sub)
         return output
 
@@ -53,17 +69,18 @@ class Solution:
         else:
             output = []
             for i, n in enumerate(nums):
-                output.extend([[n] + sub for sub in self.permute(nums[:i] + nums[i+1:])])
+                output.extend([[n] + sub for sub in self.permute_verbose(nums[:i] + nums[i+1:])])
+            return output
 
 
 if __name__ == '__main__':
     test_cases = [
-        ([1, 2, 3], [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]),
+        ([1, 2, 3], None),
     ]
 
     for test_case in test_cases:
         print('case:', test_case)
-        output = Solution().permute(test_case[0])
+        output = Solution().permute_recursive(test_case[0])
         print('output:', output)
-        assert sorted([tuple(x) for x in output]) == sorted([tuple(x) for x in test_case[1]])
+        assert sorted([tuple(x) for x in output]) == sorted([tuple(x) for x in itertools.permutations(test_case[0])])
 
