2d23d6a5ce874c574756fa24a483e4af2eba0d14
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-18 13:58:55 -0400

    LeetCode 344.

--- src/lt_344.py
+++ src/lt_344.py
@@ -1,4 +1,17 @@
-# https://leetcode.com/problems/reverse-string/
+"""
+https://leetcode.com/problems/reverse-string
+
+Related:
+  - lt_345_reverse-vowels-of-a-string
+  - lt_541_reverse-string-ii
+"""
+
+"""
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+"""
 
 class Solution:
     def reverseString(self, s):
