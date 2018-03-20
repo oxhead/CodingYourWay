a7d753fc05c132886c64f9f0c6f56549ab1b80e1
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-20 17:43:44 -0400

    LeetCode 124.

---
+++ src/lt_124.py
@@ -0,0 +1,87 @@
+"""
+https://leetcode.com/problems/binary-tree-maximum-path-sum
+https://leetcode.com/problems/path-sum
+
+Related:
+  - lt_112_path-sum
+  - lt_129_sum-root-to-leaf-numbers
+  - lt_666_path-sum-iv
+  - lt_687_longest-univalue-path
+"""
+
+"""
+Given a binary tree, find the maximum path sum.
+
+For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+
+For example:
+Given the below binary tree,
+
+       1
+      / \
+     2   3
+
+Return 6. 
+"""
+
+from utils import parse_tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        # Time: O(n)
+        # Space: O(h)
+        max_sum = float('-inf')
+        def max_path_sum(node):
+            nonlocal max_sum
+            if not node: return float('-inf')
+            left = max(0, max_path_sum(node.left))
+            right = max(0, max_path_sum(node.right))
+            max_sum = max(max_sum, node.val + left + right)
+            return node.val + max(left, right)
+        if not root: return 0
+        max_path_sum(root)
+        return max_sum
+
+    def maxPathSum_failed():
+        def max_path_sum(node, total):
+            if not node: return float('-inf')
+            if node.left and node.right:
+                s1 = max_path_sum(node.left, total + node.left.val)
+                s2 = max_path_sum(node.right, total + node.right.val)
+                return max(total, s1, s2, s1 + s2 - total)
+            elif node.left:
+                return max(total, max_path_sum(node.left, total + node.left.val))
+            elif node.right:
+                return max(total,  max_path_sum(node.right, total + node.right.val))
+            else:
+                return total
+
+        if not root: return float('-inf')
+        return max(max_path_sum(root, root.val), self.maxPathSum(root.left), self.maxPathSum(root.right))
+
+if __name__ == '__main__':
+    test_cases = [
+        ([], 0),
+        ([1, 2, 3], 6),
+        ([-2, -1], -1),
+        ([1, -2, -3, 1, 3, -2, None, -1], 3),
+        ([5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, 1], 48),
+    ]
+
+    for test_case in test_cases:
+        print('case:', test_case)
+        output = Solution().maxPathSum(parse_tree(test_case[0]))
+        print('output:', output)
+        assert output == test_case[1]
+
