d1251fc818a1068c2f0859aa29cf2895c8f3856b
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-20 11:52:07 -0400

    LeetCode 112.

--- src/lt_112.py
+++ src/lt_112.py
@@ -45,7 +45,7 @@ class Solution:
         if not root: return False
         if not root.left and not root.right: return root.val == sum
         return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
-        
+
 
 if __name__ == '__main__':
     test_cases = [
