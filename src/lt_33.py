2a2c9cff77e198f6a758bf3a27fe17c4b0018fa3
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-28 01:55:00 -0400

    Update.

--- src/lt_33.py
+++ src/lt_33.py
@@ -31,16 +31,16 @@ class Solution:
             mid = left + (right - left) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > nums[right]:
-                if nums[left] <= target < nums[mid]:
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target <=  nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
-                if nums[mid] < target <= nums[right]:
+                if nums[mid] <= target <= nums[right]:
                     left = mid + 1
                 else:
-                    right = mid - 1 
+                    right = mid - 1
         return -1
 
     def search_naive(self, nums, target):
