fcf5800d19906626bf443b69718fdb3b7890c8a1
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-28 10:44:06 -0400

    LeetCode 35.

---
+++ src/lt_35.py
@@ -0,0 +1,68 @@
+"""
+https://leetcode.com/problems/search-insert-position
+
+Related:
+  - lt_278_first-bad-version
+"""
+
+"""
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+
+Example 3:
+
+Input: [1,3,5,6], 7
+Output: 4
+
+Example 1:
+
+Input: [1,3,5,6], 0
+Output: 0
+"""
+
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # Time: O(logn)
+        # Space: O(1)
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
+
+if __name__ == '__main__':
+    test_cases = [
+        (([1,3,5,6], 5), 2),
+        (([1,3,5,6], 2), 1),
+        (([1,3,5,6], 7), 4),
+        (([1,3,5,6], 0), 0),
+    ]
+
+    for test_case in test_cases:
+        print('case:', test_case)
+        output = Solution().searchInsert(*test_case[0])
+        print('output:', output)
+        assert output == test_case[1]
+
