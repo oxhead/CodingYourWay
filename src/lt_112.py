041e5998fa527bae63d572890d2a705b87b5ede9
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-29 11:08:53 -0400

    Update.

--- src/lt_112.py
+++ src/lt_112.py
@@ -42,6 +42,8 @@ class Solution:
         :type sum: int
         :rtype: bool
         """
+        # Time: O(n)
+        # Space: O(h)
         if not root: return False
         if not root.left and not root.right: return root.val == sum
         return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
