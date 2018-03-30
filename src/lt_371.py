cbbc07444b9f4ca580a7560a17e38f625f15f0bd
Chin-Jung Hsu <kmscout@gmail.com>
2018-03-30 16:30:22 -0400

    Update.

--- src/lt_371.py
+++ src/lt_371.py
@@ -1,12 +1,9 @@
 """
-https://leetcode.com/problems/sum-of-two-integers/description/
+https://leetcode.com/problems/sum-of-two-integers
 
 Related:
-  - lt_2
+  - lt_2_add-two-numbers
 
-Complexity:
-  - Time: O(1)
-  - Space: O(1)
 
 Reference:
   - https://github.com/kamyu104/LeetCode/blob/master/Python/sum-of-two-integers.py
@@ -27,16 +24,19 @@ class Solution:
         :type b: int
         :rtype: int
         """
+        # Time: O(1)
+        # Space: O(1)
+        # https://github.com/kamyu104/LeetCode/blob/master/Python/sum-of-two-integers.py
         MAX = 0x7FFFFFFF
         mask = 0xFFFFFFFF
-        # print('a: {0:032b}'.format(a & mask))
-        # print('b: {0:032b}'.format(b & mask))
+        #print('a: {0:032b}'.format(a & mask))
+        #print('b: {0:032b}'.format(b & mask))
         while b:
             # use XOR to get different bits
             # use AND and left shift to get carry bits
             a, b = (a ^ b) & mask, ((a & b) << 1) & mask
-            # print('a: {0:032b}'.format(a))
-            # print('b: {0:032b}'.format(b))
+            #print('a: {0:032b}'.format(a))
+            #print('b: {0:032b}'.format(b))
         # print('X: {0:032b}'.format(a))
         # ~: -x - 1
         # a ^ mask: x 
