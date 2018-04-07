7bc1154e86480679f1d320469cf33265ea9a741a
Chin-Jung Hsu <kmscout@gmail.com>
2018-04-07 17:38:23 -0400

    LeetCode 176.

---
+++ src/lt_176.sql
@@ -0,0 +1,28 @@
+"""
+https://leetcode.com/problems/second-highest-salary
+
+Related:
+"""
+
+"""
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+"""
+
+# Write your MySQL query statement below
+SELECT (SELECT MAX(Salary) FROM Employee WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee)) SecondHighestSalary;
