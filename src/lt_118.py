113dd2c829bbacae564af9dd91a0e80055dbd8d7
Chin-Jung Hsu <kmscout@gmail.com>
2018-04-07 17:30:41 -0400

    Update.

--- src/lt_118.py
+++ src/lt_118.py
@@ -2,11 +2,7 @@
 https://leetcode.com/problems/pascals-triangle
 
 Related:
-  - lt_119
-
-Complexity:
-  - Time: O()
-  - Space: O()
+  - lt_119_pascals-triangle-ii
 """
 
 """
@@ -30,6 +26,8 @@ class Solution:
         :type numRows: int
         :rtype: List[List[int]]
         """
+        # Time: O(n^2)
+        # Space: O(1)
         if not numRows: return []
         output = [[1]]
         for i in range(1, numRows):
@@ -39,6 +37,17 @@ class Solution:
                 elif j == i: row.append(output[i-1][-1])
                 else: row.append(output[i-1][j-1] + output[i-1][j])
             output.append(row)
+        return output
+
+    def generate_terse(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows < 1: return []
+        output = [[1]]
+        for _ in range(1, numRows):
+            output.append([x + y for x, y in zip([0] + output[-1], output[-1] + [0])])
         return output
 
 
