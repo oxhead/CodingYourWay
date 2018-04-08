d61dcef8e2bbd1de9712530252f4659d32592aee
Chin-Jung Hsu <kmscout@gmail.com>
2018-04-08 12:32:07 -0400

    Update.

--- src/lt_81.py
+++ src/lt_81.py
@@ -35,7 +35,7 @@ class Solution:
             if nums[mid] == target: return True
             elif nums[left] == nums[mid]:
                 left += 1
-            elif nums[left] < nums[mid]:
+            elif nums[left] <= nums[mid]:
                 if nums[left] <= target <= nums[mid]:
                     right = mid - 1
                 else:
@@ -46,6 +46,34 @@ class Solution:
                 else:
                     right = mid - 1
         return False
+
+    def search_optimized(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target: return True
+            # shrink the search space?
+            elif nums[left] == nums[mid] and nums[mid] == nums[right]:
+                left += 1
+                right -= 1
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return False
+
+
 
         
 
