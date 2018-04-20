0b4d8fac31e318ba6523f99da3b3b316234d5451
Chin-Jung Hsu <kmscout@gmail.com>
2018-04-19 22:53:28 -0400

    Update.

--- src/lt_219.py
+++ src/lt_219.py
@@ -45,11 +45,10 @@ class Solution:
         return False
 
 
-
 if __name__ == '__main__':
     test_cases = [
-        #(([], 0), False),
-        #(([1, 2, 1, 1, 2, 3, 1], 2), True),
+        (([], 0), False),
+        (([1, 2, 1, 1, 2, 3, 1], 2), True),
         (([1, 2, 3, 4, 5, 1], 2), False),
         (([99, 99], 2), True),
         (([-1, -1], 1), True),
