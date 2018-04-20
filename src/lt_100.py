234ecb1672fd7acb8fcbb4a82e536e5d5ac7038e
Chin-Jung Hsu <kmscout@gmail.com>
2018-04-20 10:53:00 -0400

    Update.

--- src/lt_100.py
+++ src/lt_100.py
@@ -56,8 +56,10 @@ class Solution:
         :type q: TreeNode
         :rtype: bool
         """
+        # Time: O(n)
+        # Space: O(h)
         if not p and not q: return True
-        elif (p and not q) or (not p and q): return False
+        elif not p or not q: return False
         elif p.val != q.val: return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
