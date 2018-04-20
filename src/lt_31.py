effd47b8eb51f52779d888c537b99a2f1a2b8cf0
Chin-Jung Hsu <kmscout@gmail.com>
2018-04-20 10:47:23 -0400

    Update.

--- src/lt_31.py
+++ src/lt_31.py
@@ -30,6 +30,37 @@ class Solution:
         # Time: O(n)
         # Space: O(1)
         # http://fisherlei.blogspot.sg/2012/12/leetcode-next-permutation.html
+        # Example: [6, 8, 7, 4, 3, 2]
+        # 0 1 2 3 4 5 (index)
+        # 6 8 7 4 3 2 (nums)
+        # 0           (m)
+        #     2       (n)
+        # 7 8 6 4 3 2 (swap nums[m] and nums[n])
+        # 7 2 3 4 6 8 (reverse slice)
+        m = n = -1
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                m = i
+                break
+        if m == -1:
+            nums.reverse()
+            return
+        for i in range(len(nums)-1, m, -1):
+            if nums[i] > nums[m]:
+                n = i
+                break
+        nums[m], nums[n] = nums[n], nums[m]
+        # reverse the slice
+        nums[m+1:] = nums[:m:-1]
+
+    def nextPermutation_verbose(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        # Time: O(n)
+        # Space: O(1)
+        # http://fisherlei.blogspot.sg/2012/12/leetcode-next-permutation.html
         index_partition = index_change = -1
         for i in range(len(nums) - 1, 0, -1):
             if nums[i] > nums[i - 1]:
@@ -50,7 +81,7 @@ class Solution:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
-       
+
 
 if __name__ == '__main__':
     test_cases = [
@@ -61,6 +92,7 @@ if __name__ == '__main__':
         ([3, 1, 2], [3, 2, 1]),
         ([3, 2, 1], [1, 2, 3]),
         ([1, 1, 5], [1, 5, 1]),
+        ([6, 8, 7, 4, 3, 2], [7, 2, 3, 4, 6, 8]),
     ]
 
     for test_case in test_cases:
