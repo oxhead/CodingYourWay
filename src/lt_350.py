8efdab074b6f5b11fe9b0d2aca9369712ebee9b7
Chin-Jung Hsu <kmscout@gmail.com>
2018-04-27 23:40:01 -0400

    Update.

--- src/lt_350.py
+++ src/lt_350.py
@@ -42,6 +42,55 @@ class Solution:
                     break
         return output
 
+    def intersect_twopointers(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        # Time: O(m + n)
+        # Space: O(1)
+        nums1.sort()
+        nums2.sort()
+        output = []
+        p1, p2 = 0, 0
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] == nums2[p2]:
+                output.append(nums1[p1])
+                p1 += 1
+                p2 += 1
+            elif nums1[p1] < nums2[p2]:
+                p1 += 1
+            else:
+                p2 += 1
+        return output
+
+    def intersect_binarysearch(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        # Time: O(min(m, n) * log(max(m, n)))
+        # Space: O(1)
+        def binary_search(nums, left, right, target):
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] >= target: right = mid - 1
+                else: left = mid + 1
+            return left
+        if len(nums1) > len(nums2): return self.intersect_binarysearch(nums2, nums1)
+        nums1.sort()
+        nums2.sort()
+        output = []
+        left = 0
+        for n in nums1:
+            left = binary_search(nums2, left, len(nums2) - 1, n)
+            if left != len(nums2) and nums2[left] == n:
+                output.append(n)
+                left += 1
+        return output 
+
 
 if __name__ == '__main__':
     test_cases = [
